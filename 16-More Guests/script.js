1b79e01b8d41a0f213842f46870e4865f32f4478
Faizan Tayyab <Faizan1421@gmail.com>
2022-10-01 22:50:25 +0500

    completed till exercise 21

--- 16-More Guests/script.js
+++ 16-More Guests/script.js
@@ -1,26 +1,19 @@
-// Q15-Changing Guest List: You just heard that one of your guests can’t make the dinner, so you need to send out a new set of invitations. You’ll have to think of someone else to invite.
-
-// • Start with your program from Exercise 14. Add a print statement at the end of your program stating the name of the guest who can’t make it.
-
-// • Modify your list, replacing the name of the guest who can’t make it with the name of the new person you are inviting.
-
-// • Print a second set of invitation messages, one for each person who is still in your list.
+// Q16-More Guests: You just found a bigger dinner table, so now more space is available. Think of three more guests to invite to dinner.
 
 let guests = ["zain", "Basit", "Ali"];
 let invitations = guests.map((item) => {
   return `${item} You are Invited To the Dinner Tonight `;
 });
-console.log(invitations.join("\n"));
-// Start with your program from Exercise 14. Add a print statement at the end of your program stating the name of the guest who can’t make it.
-
 console.log(`${guests[1]} is not available for the dinner`);
-
-// Modify your list, replacing the name of the guest who can’t make it with the name of the new person you are inviting.
-
 guests[1] = "Salik";
 
-// Print a second set of invitation messages, one for each person who is still in your list.
+// Start with your program from Exercise 15. Add a print statement to the end of your program informing people that you found a bigger dinner table.
 
+console.log("We have More Space Availabe for more Guests on Our Dinner Table");
+// Add one new guest to the beginning of your array.
+guests.unshift("Farhan");
+// Add one new guest to the middle of your array. • Use append() to add one new guest to the end of your list. • Print a new set of invitation messages, one for each person in your list.
+guests.splice(2, 0, "Nadir");
 let newInvitations = guests.map((item) => {
   return `${item} You are Invited To the Dinner Tonight `;
 });
@@ -30,11 +23,17 @@ console.log(newInvitations.join("\n"));
 let text1 = document.createElement("h3");
 let text2 = document.createElement("h3");
 let text3 = document.createElement("h3");
+let text4 = document.createElement("h3");
+let text5 = document.createElement("h3");
 
 text1.innerText = newInvitations[0];
 text2.innerText = newInvitations[1];
 text3.innerText = newInvitations[2];
+text4.innerText = newInvitations[3];
+text5.innerText = newInvitations[4];
 
 document.body.appendChild(text1);
 document.body.appendChild(text2);
 document.body.appendChild(text3);
+document.body.appendChild(text4);
+document.body.appendChild(text5);
---
+++ 19-Dinner Guests/script.js
@@ -0,0 +1,19 @@
+// Q19-Dinner Guests: Working with one of the programs from Exercises 14 through 18, print a message indicating the number of people you are inviting to dinner.
+
+let guests = ["zain", "Basit", "Ali"];
+let invitations = guests.map((item) => {
+  return `${item} You are Invited To the Dinner Tonight `;
+});
+console.log(invitations.join("\n"));
+
+let text1 = document.createElement("h3");
+let text2 = document.createElement("h3");
+let text3 = document.createElement("h3");
+
+text1.innerText = invitations[0];
+text2.innerText = invitations[1];
+text3.innerText = invitations[2];
+
+document.body.appendChild(text1);
+document.body.appendChild(text2);
+document.body.appendChild(text3);
