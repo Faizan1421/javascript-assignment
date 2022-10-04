2753b03b9ed8c8b795ffe90e7070989b1892799c
Faizan Tayyab <Faizan1421@gmail.com>
2022-10-04 15:06:53 +0500

    updated 45 assignment

--- 45-Cars/script.js
+++ 45-Cars/script.js
@@ -15,3 +15,31 @@ let suzukiMehran = carInfo("Suzuki", "Mehran", (year = 2018));
 console.log(hondaAccord);
 console.log(toyotaAltis);
 console.log(suzukiMehran);
+//////////////////////////////////////////////////////////////////////////////////
+// //////////////Method 2//////////////////////
+function carInfo1(manufacturer, model, ...optional) {
+  let makeCar = {
+    manufacturer,
+    model,
+    color: optional[0],
+    year: optional[1],
+  };
+  return makeCar;
+}
+
+let hondaAccord1 = carInfo1(
+  "honda",
+  "accord",
+  (color = "white"),
+  (year = 2022)
+);
+let toyotaAltis1 = carInfo1(
+  "Toyota",
+  "Altis",
+  (color = "black"),
+  (year = 2022)
+);
+let suzukiMehran1 = carInfo1("Suzuki", "Mehran", (color = null), (year = 2018));
+console.log(hondaAccord1);
+console.log(toyotaAltis1);
+console.log(suzukiMehran1);
