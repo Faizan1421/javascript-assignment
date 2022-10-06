3e281ae5bee8dda656a3f4f7318916538f8e46aa
Faizan Tayyab <Faizan1421@gmail.com>
2022-10-06 12:51:06 +0500

    updated

--- 44-Sandwiches/script.js
+++ 44-Sandwiches/script.js
@@ -1,8 +1,12 @@
 // Q44-Sandwiches: Write a function that accepts a array of items a person wants on a sandwich. The function should have one parameter that collects as many items as the function call provides, and it should print a summary of the sandwich that is being ordered. Call the function three times, using a different number of arguments each time.
 
 function makeSandwiches(...items) {
-  console.log(`Adding ${items} to Your Sandwich`);
+  for (let i = 0; i < items.length; i++) {
+    console.log(`Adding ${items[i]} to Your Sandwich`);
+  }
+  console.log(`Your sandwich is Ready`);
+  console.log(`------------------------`);
 }
 makeSandwiches("roast beef");
 makeSandwiches("turkey", "apple slices", "honey mustard");
-makeSandwiches("peanut, butter");
+makeSandwiches("peanut", "butter");
